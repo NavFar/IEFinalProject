0613683a9f634bd03e6182bc20d24748471121dd
NavidFarahmand <NavidFarahmand75@gmail.com>
2017-01-18 00:24:03 +0330

    Done with register page (fuck this shit)

--- src/NavFar/UserBundle/Controller/UserController.php
+++ src/NavFar/UserBundle/Controller/UserController.php
@@ -8,8 +8,31 @@ use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use NavFar\UserBundle\Entity\User;
 use Doctrine\ORM;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 class UserController extends Controller
 {
+
+  /**
+   * @Route("/active/{username}",name="activation")
+   * @Method({"GET"})
+   */
+  public function activationAction($username)
+  {
+    try{
+
+      $em = $this->getDoctrine()->getManager();
+      $repository = $em->getRepository('NavFarUserBundle:User');
+      $user=$repository->findOneByUsername($username);
+      if(!$user)
+      throw new \Exception('user not found');
+      $user->setActive(1);
+      $em->flush();
+    }catch(\Exception $e){
+      return $this->render('NavFarUserBundle:User:activation.html.twig',array("error"=>1));
+    }
+      return $this->render('NavFarUserBundle:User:activation.html.twig');
+  }
     /**
      * @Route("/register",name="registerGET")
      * @Method({"GET"})
@@ -49,18 +72,37 @@ class UserController extends Controller
       try{
       $newUser = new User();
       $newUser->setUsername($username);
-      $newUser->setEmail($email);
+      $newUser->setEmail(strtolower($email));
       $newUser->setPassword($password);
+      $newUser->setActive("false");
       $em = $this->getDoctrine()->getManager();
       $em->persist($newUser);
       $em->flush();
-      $flagError = 0;
     }catch(\Exception $e){
       $flagError=1;
       array_push($ErrorMessage,"نام کاربری یا ایمیل تکراری است."."\n");
     }
-      if(!$flagError)//should save the information to session and redirect to homepage
-      return $this->render('NavFarUserBundle:User:register.html.twig',array("Error"=>0));
+      if(!$flagError){
+
+        $address = $this->generateUrl('activation',
+                                      array('username' => $username),
+                                      UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $message = \Swift_Message::newInstance()
+                ->setSubject('Activation')
+                ->setFrom('navidfarahmand75@gmail.com')
+                ->setTo($email)
+                ->setBody(
+                    $this->renderView(
+                        'NavFarUserBundle:Email:activate.html.twig',
+                        array("username"=>$username,
+                              "address"=>$address)
+                    )
+                )
+            ;
+            $this->get('mailer')->send($message);
+      return $this->render('NavFarUserBundle:User:register.html.twig',array("In"=>1));
+    }
       else
       return $this->render('NavFarUserBundle:User:register.html.twig',array("Error"=>1,
                                                                             "ErrorMessages"=>$ErrorMessage));
